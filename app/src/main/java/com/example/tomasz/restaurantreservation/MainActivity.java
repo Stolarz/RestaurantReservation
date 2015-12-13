42404cea04b231a1723fe2f979f7ed213a012dc3
Tomasz Solik <tomek.solik@gmail.com>
2015-12-13 18:31:06 +0100

    Dodano podstawową listę z nazwami restauracji.

--- app/src/main/java/com/example/tomasz/restaurantreservation/MainActivity.java
+++ app/src/main/java/com/example/tomasz/restaurantreservation/MainActivity.java
@@ -1,126 +1,52 @@
 package com.example.tomasz.restaurantreservation;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.parse.ParseUser;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
-    private TextView _drawerUsername;
-    private TextView _drawerEmail;
-    ParseUser currentUser = ParseUser.getCurrentUser();
+public class MainActivity extends AppCompatActivity {
+
+    private ListView _lv;
+    ArrayAdapter<String> adapter;
+    String[] parseResult = {""};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        settingUp();
         setContentView(R.layout.activity_main);
-        _drawerUsername.setText(currentUser.getUsername());
-        Log.e("Error3: ", _drawerUsername.getText().toString());
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("RestaurantInfo");
+        query.findInBackground(new FindCallback<ParseObject>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void done(List<ParseObject> list, com.parse.ParseException e) {
+                if (e == null) {
+                    parseResult = new String[list.size()];
+                    for (int index = 0; index < list.size(); index++) {
+                        parseResult[index] = list.get(index).getString("Name");
+                        adapter.add(parseResult[index]);
+                        Log.e(": ", parseResult[index].toString());
+                    }
+                    adapter.notifyDataSetChanged();
+                } else {
+                    Log.e("Parse query: ", e.getMessage());
+                }
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
-
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-    }
-
-    private void settingUp() {
-        setContentView(R.layout.nav_header_main);
-        _drawerUsername = (TextView) findViewById(R.id.DrawerUsername);
-        Log.e("Error: ",_drawerUsername.getText().toString());
-        _drawerUsername.setText(currentUser.getUsername());
-        Log.e("Error2: ", _drawerUsername.getText().toString());
-        _drawerEmail = (TextView) findViewById(R.id.DrawerEmail);
-        _drawerEmail.setText(currentUser.getEmail());
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        _lv = (ListView)findViewById(R.id.list);
+        _lv.setAdapter(adapter);
     }
 }
